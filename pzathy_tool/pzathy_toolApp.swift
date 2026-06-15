7dd8c7aa1401ab0f2fc663f970a4f07d54dfa7de
puthsithamouern <puthsithamouern@gmail.com>
2026-06-15 21:53:44 +0700

    Initial Commit

---
+++ pzathy_tool/pzathy_toolApp.swift
@@ -0,0 +1,17 @@
+//
+//  pzathy_toolApp.swift
+//  pzathy_tool
+//
+//  Created by Puthsitha Moeurn on 15/6/26.
+//
+
+import SwiftUI
+
+@main
+struct pzathy_toolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
